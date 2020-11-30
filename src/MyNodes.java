60b62088c91820b8be5a5f8b16b43a4eeb251a7e
vismaya01 <kvvismaya98@gmail.com>
2020-11-30 21:59:06 +0530

    Added: Simple Linked List with 3 number

--- src/MyNodes.java
+++ src/MyNodes.java
@@ -1,4 +1,18 @@
 
-public class MyNodes {
-
+public class MyNodes<K> {
+	private K key;
+	private MyNodes next;
+	
+	public MyNodes(K key) {
+		this.key = key;
+		this.next = null;
+	}
+	
+	public MyNodes getNext() {
+		return next;
+	}
+	
+	public void setNext(MyNodes next) {
+		this.next = next;
+	}
 }
